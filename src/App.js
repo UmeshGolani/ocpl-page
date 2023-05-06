d2d2b579c51cb0586aee4075c5d18ea5f8cccba4
UmeshGolani <35754561+UmeshGolani@users.noreply.github.com>
2023-05-06 23:29:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+import LoginForm from './Components/LoginForm';
+import RegisterForm from './Components/RegistrationForm';
+import DataForm from './Components/DataForm';
+
+const App = () => {
+  const [data, setData] = useState({
+    property1: '',
+    property2: ''
+  });
+
+  const handleDataSubmit = (e) => {
+    e.preventDefault();
+    axios.post('http://91.203.132.6/handleData', {data})
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
+  };
+
+  const handleDataInputChange = (e) => {
+    setData({...data, [e.target.name]: e.target.value});
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+  <Routes>
+    <Route path="/" element={<LoginForm />} />
+    <Route path="/register" element={<RegisterForm />} />
+    <Route path="/data" element={<DataForm onSubmit={handleDataSubmit} onChange={handleDataInputChange} data={data} />} />
+  </Routes>
+</Router>
   );
-}
+};
 
 export default App;
